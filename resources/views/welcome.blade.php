190f8ccc30efe51de30fd5b5377bfdb0811c839f
iSatVeerSingh <developer.satveer@gmail.com>
2024-04-25 15:13:12 +0530

    login page modified

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="icon" href="/logo.png" type="image/png" />
-  <link rel="apple-touch-icon" href="/logo.png" />
-  <meta name="msapplication-TileColor" content="#FFFFFF" />
-  <meta name="theme-color" content="#000000" />
-  <title>Inspection Report App</title>
-  <meta name="description" content="Inspection Report App for Correct Inspections, Australia" />
-  <script type="module" crossorigin src="/assets/index-DGQRTwq1.js"></script>
-  <link rel="stylesheet" crossorigin href="/assets/index-Nno4m7lO.css">
-  <link rel="manifest" href="/manifest.webmanifest">
-</head>
-
-<body>
-  <div id="root"></div>
-</body>
-
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" href="/logo.png" type="image/png" />
+    <link rel="apple-touch-icon" href="/logo.png" />
+    <meta name="msapplication-TileColor" content="#FFFFFF" />
+    <meta name="theme-color" content="#000000" />
+    <title>Inspection Report App</title>
+    <meta
+      name="description"
+      content="Inspection Report App for Correct Inspections, Australia"
+    />
+    <script type="module" crossorigin src="/assets/index-Ca6YgTfx.js"></script>
+    <link rel="stylesheet" crossorigin href="/assets/index-Nno4m7lO.css">
+  <link rel="manifest" href="/manifest.webmanifest"></head>
+  <body>
+    <div id="root"></div>
+  </body>
 </html>
