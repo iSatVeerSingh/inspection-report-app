3241984f919dd7022e1021d371d4b6d581cad04a
iSatVeerSingh <developer.satveer@gmail.com>
2024-04-26 13:22:19 +0530

    sync modified

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
       name="description"
       content="Inspection Report App for Correct Inspections, Australia"
     />
-    <script type="module" crossorigin src="/assets/index-Ca6YgTfx.js"></script>
+    <script type="module" crossorigin src="/assets/index-Bt87n-LR.js"></script>
     <link rel="stylesheet" crossorigin href="/assets/index-Nno4m7lO.css">
   <link rel="manifest" href="/manifest.webmanifest"></head>
   <body>
